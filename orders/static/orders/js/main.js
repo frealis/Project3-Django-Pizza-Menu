8f7256b57c65cce20db715fc2ad2c7f256e71de0
frealis <36343653+frealis@users.noreply.github.com>
2019-03-05 00:51:26 -0500

    Add extra pop-up for Subs

--- orders/static/orders/js/main.js
+++ orders/static/orders/js/main.js
@@ -1,6 +1,6 @@
 // --------------------- SELECT ITEM ---------------------
 
-// Variables
+// Global variables
 number_of_selections = 0;
 current_selection = "";
 previous_selection = "";
@@ -12,6 +12,7 @@ function select_item(obj) {
   tr_id = obj.getAttribute('data-tr_id');
   td_name = obj.getAttribute('name');
   data_toppings = obj.getAttribute('data-toppings');
+  data_extras = obj.getAttribute('data-extras');
   number_of_selections++;
   current_selection = td_name;
 
@@ -25,23 +26,31 @@ function select_item(obj) {
   if (current_selection === previous_selection) {
 
     // Hide toppings & de-select all checkboxes
-    if (number_of_selections > 1 || data_toppings === 'false') {
-      hide_toppings();
+    if (number_of_selections > 1) {
+      hide_all();
 
     // Show toppings
-    } else {
-      show_toppings(tr_id, data_toppings);
+    } else if (data_toppings === 'true') {
+      show_toppings(tr_id);
+
+    // Show extras
+    } else if (data_extras === 'true') {
+      show_extras(tr_id);
     };
 
   // User clicks on an item that was -not- already selected
   } else {
 
     // Hide toppings & de-select all checkboxes
-    hide_toppings();
+    hide_all();
 
     // Show toppings
     if (data_toppings === 'true') {
-      show_toppings(tr_id, data_toppings);
+      show_toppings(tr_id);
+
+    // Show extras
+    } else if (data_extras === 'true') {
+      show_extras(tr_id);
     };
 
     // Re-activate new checkbox selection
@@ -53,41 +62,23 @@ function select_item(obj) {
   previous_selection = current_selection;
 };
 
-// --------------------- HIDE TOPPINGS ---------------------
-
-// Hide toppings
-function hide_toppings() {
-  if (number_of_selections > 1) {
-    number_of_selections = 0;
-    var toppings = document.querySelector('.td_toppings');
-    if (toppings) {
-      toppings.parentNode.removeChild(toppings);
-      toppings_visible = false;
-    }
-    var input = document.getElementsByTagName('input')
-    for (i = 0; i < input.length; i++) {
-      input[i].checked = false;
-    };
-  };
-};
-
 // --------------------- SHOW EXTRAS ---------------------
 
-// // List all extras here
-// extras_items = 'extras items go here';
-
-// // Create a new row, <tr>, that includes list of extras, hidden by default.
-// const tr_extras = document.createElement('tr');
-// const td_extras = document.createElement('td');
-// td_extras.className = 'extras';
-// td_extras.innerHTML = extras_items;
-// td_extras.style.display = 'none';
-// tr_extras.append(td_extras);
+// List all extras here
+extras_items = 'extras items go here';
 
 // Show extras
-extras_visible = false;
-function extras() {
-  //
+function show_extras(tr_id) {
+
+  // Create a new row, <tr>, that includes list of extras, hidden by default.
+  const tr_extras = document.createElement('tr');
+  const td_extras = document.createElement('td');
+  td_extras.innerHTML = extras_items;
+  tr_extras.className = 'tr_extras';
+  tr_extras.append(td_extras);
+
+  // Add toppings row <tr> to DOM. 
+  document.querySelector('tbody').insertBefore(tr_extras, tbody.childNodes[index(tr_id)]);
 };
 
 // --------------------- SHOW TOPPINGS ---------------------
@@ -96,28 +87,55 @@ function extras() {
 toppings_items = 'toppings items go here';
 
 // Show toppings
-function show_toppings(tr_id, data_toppings) {
+function show_toppings(tr_id) {
 
-  if (data_toppings === 'true') {
+  // Create a new row, <tr>, that includes list of toppings, hidden by default.
+  const tr_toppings = document.createElement('tr');
+  const td_toppings = document.createElement('td');
+  td_toppings.innerHTML = toppings_items;
+  tr_toppings.className = 'tr_toppings';
+  tr_toppings.append(td_toppings);
 
-    // Create a new row, <tr>, that includes list of toppings, hidden by default.
-    const tr_toppings = document.createElement('tr');
-    const td_toppings = document.createElement('td');
-    td_toppings.innerHTML = toppings_items;
-    tr_toppings.className = 'td_toppings';
-    tr_toppings.append(td_toppings);
+  // Add toppings row <tr> to DOM. 
+  document.querySelector('tbody').insertBefore(tr_toppings, tbody.childNodes[index(tr_id)]);
+};
 
-    // Figure out the index of the HTML child objects of <tbody>.
-    var array = [];
-    var index = 0;
-    for (let i = 0; i < tbody.childNodes.length; i++) {
-      array[i] = tbody.childNodes[i];
-      if (array[i].id === tr_id) {
-        index = i + 1;
-      };
+// --------------------- FIND INDEX ---------------------
+
+// Figure out the index of the HTML child objects of <tbody>.
+function index (tr_id) {
+  var array = [];
+  var index = 0;
+  for (let i = 0; i < tbody.childNodes.length; i++) {
+    array[i] = tbody.childNodes[i];
+    if (array[i].id === tr_id) {
+      index = i + 1;
     };
+  };
+  return index;
+}
 
-    // Add toppings row <tr> to DOM. 
-    document.querySelector('tbody').insertBefore(tr_toppings, tbody.childNodes[index]);
+// --------------------- HIDE ALL ---------------------
+
+// Hide toppings and extras
+function hide_all() {
+  number_of_selections = 0;
+
+  // Hide extras
+  var extras = document.querySelector('.tr_extras');
+  if (extras) {
+    extras.parentNode.removeChild(extras);
+  }
+
+  // Hide toppings
+  var toppings = document.querySelector('.tr_toppings');
+  if (toppings) {
+    toppings.parentNode.removeChild(toppings);
+  }
+
+  // Uncheck all checkboxes
+  var input = document.getElementsByTagName('input')
+  for (i = 0; i < input.length; i++) {
+    input[i].checked = false;
   };
 };
